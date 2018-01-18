5accee95e82382eb2f0195f69055e0c90af3158b
JeremyPace <30711940+JeremyPace@users.noreply.github.com>
2018-01-18 16:56:59 -0700

    added window() and createWindow()

--- tools/Settings.java
+++ tools/Settings.java
@@ -1,2 +1,24 @@
-public class Settings {
+import javax.swing.JFrame;
+public class Settings();
+
+   public static void main(String[] args) {
+   
+     private void createWindow(boolean tf, String name, int x, int y) 
+     {
+       if (tf == true) 
+       {
+         window(tf, name, x, y);
+       }  
+     }
+    private void window(String title, int x, int y) 
+    {
+      
+      JFrame frame = new JFrame();
+      frame.setTitle(title);
+      frame.setSize(x, y);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+    }
+    }
+   
 } 
