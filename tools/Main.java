0e4ac8c3b268c688e2ab252f74c19a25041dbe1f
JeremyPace <30711940+JeremyPace@users.noreply.github.com>
2018-01-18 17:09:06 -0700

    removed TF boolean

--- tools/Main.java
+++ tools/Main.java
@@ -3,12 +3,9 @@ public class Main();
 
    public static void main(String[] args) {
    
-     private void createWindow(boolean tf, String name, int x, int y) 
+     private void createWindow(String name, int x, int y) 
      {
-       if (tf == true) 
-       {
-         window(tf, name, x, y);
-       }  
+        window(name, x, y);
      }
     private void window(String title, int x, int y) 
     {
